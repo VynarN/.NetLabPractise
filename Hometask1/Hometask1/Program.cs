6aab8f330aca67a2e0588beb09e496f4abd41ad9
Nazar Vynar <vynar.n.i@gmail.com>
2019-08-08 18:40:52 +0300

    first commit

---
+++ Hometask1/Hometask1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hometask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
